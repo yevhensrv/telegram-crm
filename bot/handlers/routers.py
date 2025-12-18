7200f2817801d9c50430054b35d4eea46b8e4b66
yevhensrv <vnderlord@gmail.com>
2025-12-18 14:01:34 +0200

    Update routers.py

--- bot/handlers/routers.py
+++ bot/handlers/routers.py
@@ -1,1 +1,20 @@
+# Файл: bot/handlers/routers.py
 
+from aiogram import Router
+from . import start
+from . import tasks
+from . import workspaces
+from . import reminders
+from . import comments # НОВЫЙ ХЭНДЛЕР
+
+# Создаем главный роутер, который соберет все остальные
+main_router = Router()
+
+# Включаем все роутеры в главный
+main_router.include_router(start.router)
+main_router.include_router(workspaces.router)
+main_router.include_router(tasks.router)
+main_router.include_router(reminders.router)
+main_router.include_router(comments.router)
+
+# Мы возвращаем main_router, чтобы импортировать его в main.py
