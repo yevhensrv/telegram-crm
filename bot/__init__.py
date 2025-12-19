d9a206e1a618d1e59ee9c995a72308c9e1907ba4
yevhensrv <vnderlord@gmail.com>
2025-12-19 15:18:21 +0200

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,14 @@
 """
-Пакет бота
+Инициализация обработчиков
 """
+
+from bot.handlers import start, tasks, workspaces, reminders, comments
+
+# Список всех роутеров
+routers = [
+    start.router,
+    workspaces.router,
+    tasks.router,
+    reminders.router,
+    comments.router,
+]
